9077c542bdf16ac3ade42589af5b9fd94fb8a8f1
Michael <polli104@mail.chapman.edu>
2020-08-24 10:33:09 -0700

    fix Color(float,float,float,float)

--- nui/src/test/java/org/terasology/nui/ColorTest.java
+++ nui/src/test/java/org/terasology/nui/ColorTest.java
@@ -200,6 +200,37 @@ public class ColorTest {
     }
 
     @Test
+    public void testFloatColor() {
+        Color c1 = new Color(1.0f, 0, 0);
+        Color c2 = new Color(0, 1.0f, 0);
+        Color c3 = new Color(0, 0, 1.0f);
+        Color c4 = new Color(1.5f, 0, 1.0f);
+
+        assertEquals(255, c1.r());
+        assertEquals(0, c1.g());
+        assertEquals(0, c1.b());
+        assertEquals(255, c1.a());
+        assertEquals(Color.red, c1);
+
+        assertEquals(c2.r(), 0);
+        assertEquals(c2.g(), 255);
+        assertEquals(c2.b(), 0);
+        assertEquals(c2.a(), 255);
+        assertEquals(Color.green, c2);
+
+        assertEquals(c3.r(), 0);
+        assertEquals(c3.g(), 0);
+        assertEquals(c3.b(), 255);
+        assertEquals(Color.blue, c3);
+
+        assertEquals(c4.r(), 255);
+        assertEquals(c4.g(), 0);
+        assertEquals(c4.b(), 255);
+        assertEquals(c4.a(), 255);
+        assertEquals(Color.magenta, c4);
+    }
+
+    @Test
     public void testInvert() {
         Color c1 = new Color(255, 0, 0, 155);
 
